efec690544c0609a2fa8bfd421c019422fb321a4
Esau Matias <esaufreirematias@gmail.com>
2021-08-25 16:08:58 -0300

    Condições If e Else

---
+++ Fundamentos/4 - Introdução à JavaScript e Lógica de Programação/1 - JavaScript - Primeiros passos/Condições If e Else/scripts.js
@@ -0,0 +1,9 @@
+let pessoa = 80;
+
+if (pessoa >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!")
+} else if (pessoa < 80 && pessoa >= 60){
+    console.log("Você está na nossa lista de espera")
+} else if (pessoa < 60){
+    console.log("Você foi reprovada(o)")
+}
