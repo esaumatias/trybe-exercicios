d3826b432aa53c681f6091d5a20a4c1225fae0d7
Esau Matias <esaufreirematias@gmail.com>
2021-08-26 15:13:03 -0300

    exercicio Arrays (listas)

---
+++ Fundamentos/4 - Introdução à JavaScript e Lógica de Programação/2 - JavaScript - Array e loop For/Arrays (listas)/scripts.js
@@ -0,0 +1,11 @@
+let menu = ['home', 'serviços', 'portifolio', 'links'];
+let menuServices = '';
+menuServices = menu[1];
+
+console.log(menuServices);
+
+menuServices = menu[2];
+console.log(menuServices)
+
+menu.push('Contato')
+console.log(menu)
