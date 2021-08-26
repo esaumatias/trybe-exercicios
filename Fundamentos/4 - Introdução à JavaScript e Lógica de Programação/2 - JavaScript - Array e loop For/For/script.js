edf6515c9975f405d350932349dce9ca4d988578
Esau Matias <esaufreirematias@gmail.com>
2021-08-26 15:43:35 -0300

    exercicios For

---
+++ Fundamentos/4 - Introdução à JavaScript e Lógica de Programação/2 - JavaScript - Array e loop For/For/script.js
@@ -0,0 +1,10 @@
+let numero = 7;
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < 10; index += 1  ) {
+    console.log(numero * index)
+}
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index])
+}
