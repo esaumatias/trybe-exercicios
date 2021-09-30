218294c2f798fc7b0895ed92d1cfcb4721a2d17c
Esau Matias <esaufreirematias@gmail.com>
2021-09-30 16:50:59 -0300

    finaliza parte I e começa parte II

---
+++ Fundamentos/7 - Introdução à JavaScript ES6 e Testes Unitários/2 - objects/1 - Exercícios/parteII.js
@@ -0,0 +1,39 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Crie uma função para adicionar o turno da noite na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
+const adcTurno = (a, b, c) => {
+  a[b] = c;
+}
+adcTurno(lesson2, 'turno', 'noite');
+
+console.log(lesson2);
+
+// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const viewKeys = (obj) => {
+  const keys = Object.keys(obj);
+  return keys;
+}
+
+console.log(viewKeys(lesson3));
+
+// Crie uma função para mostrar o tamanho de um objeto.
