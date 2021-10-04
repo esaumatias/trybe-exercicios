f0c434bafead542e696170b1f201e6fdbe2eaa31
Esau Matias <esaufreirematias@gmail.com>
2021-10-04 16:18:13 -0300

    finaliza requisito 2 do exercicio

--- Fundamentos/8 - Higher Order Functions do JavaScript ES6/1 - Introdução a Higher Order Functions/1 - Exercícios/scripts.js
+++ Fundamentos/8 - Higher Order Functions do JavaScript ES6/1 - Introdução a Higher Order Functions/1 - Exercícios/scripts.js
@@ -1,4 +1,6 @@
-const gerador = (nomeCompleto) => {
+// 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+
+const geradorName = (nomeCompleto) => {
   const email = nomeCompleto.toLowerCase().split(' ').join('_');
   return {nomeCompleto, email: `${email}@trybe.com`}
 }
@@ -12,4 +14,21 @@ const newEmployees = (nome) => {
   return employees;
 };
 
-console.log(newEmployees(gerador));
+console.log(newEmployees(geradorName));
+
+// 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const geradorNumber = () => {
+  return Math.floor(Math.random() * 6)
+};
+
+
+const checkNumber = (meuNumero, sorteado) => {
+  if (meuNumero === sorteado) {
+    return 'Parabéns você ganhou';
+  } else {
+    return 'Tente novamente';
+  }
+};
+
+console.log(checkNumber(5,  geradorNumber()))
