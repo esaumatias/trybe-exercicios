7633f60bdc83b181fb327a1f5801c059305c017d
Esau Matias <esaufreirematias@gmail.com>
2021-10-06 18:44:29 -0300

    finaliza exercicio 5

--- Fundamentos/8 - Higher Order Functions do JavaScript ES6/3 - map e filter/1 - Exercícios/exercise5.js
+++ Fundamentos/8 - Higher Order Functions do JavaScript ES6/3 - map e filter/1 - Exercícios/exercise5.js
@@ -62,3 +62,17 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
+];
+
+function fantasyOrScienceFictionAuthors() {
+  const wantedGenres = ['Fantasia', 'Ficção Científica'];
+  return books
+    .filter((book) => wantedGenres.includes(book.genre))
+    .map((book) => book.author.name).sort();
+}
