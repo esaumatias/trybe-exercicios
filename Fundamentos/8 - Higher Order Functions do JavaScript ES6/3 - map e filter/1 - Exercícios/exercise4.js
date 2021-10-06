81b145f43a849470220aa62b3bf8bf6c41bddab9
Esau Matias <esaufreirematias@gmail.com>
2021-10-06 18:32:34 -0300

    finaliza exercicio 4

--- Fundamentos/8 - Higher Order Functions do JavaScript ES6/3 - map e filter/1 - Exercícios/exercise4.js
+++ Fundamentos/8 - Higher Order Functions do JavaScript ES6/3 - map e filter/1 - Exercícios/exercise4.js
@@ -62,3 +62,36 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+function oldBooksOrdered() {
+  // escreva seu código aqui
+
+  const newAray = books.filter((value) => {
+    return 2021 - value.releaseYear > 60;
+  });
+  return newAray.sort((a, b) => a.releaseYear - b.releaseYear);
+}
