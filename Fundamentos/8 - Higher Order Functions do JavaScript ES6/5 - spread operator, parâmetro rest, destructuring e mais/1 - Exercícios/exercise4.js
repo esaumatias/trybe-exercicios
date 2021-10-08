9e8e3a1afaf622e4bd40683aea7e6065a3b35574
Esau Matias <esaufreirematias@gmail.com>
2021-10-08 18:47:49 -0300

    finaliza requisito 4 do exercicio

---
+++ Fundamentos/8 - Higher Order Functions do JavaScript ES6/5 - spread operator, parâmetro rest, destructuring e mais/1 - Exercícios/exercise4.js
@@ -0,0 +1,40 @@
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
+
+const filterPeople = (pessoa) => {
+  const newPessoa = pessoa.filter(({bornIn, nationality}) => {
+    if (bornIn >= 1901 && bornIn <= 2000 && nationality === 'Australian') {
+      return pessoa;
+    }
+  });
+  return newPessoa;
+};
+
+console.log(filterPeople(people))
