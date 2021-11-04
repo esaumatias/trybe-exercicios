000dc2ebb4b11d869f9e09f490eb95d74d735bba
Esau Matias <esaufreirematias@gmail.com>
2021-11-04 14:13:23 -0300

    adiciona exercicio 1

---
+++ Desenvolvimento Front-end/11 - Componentes com Estado, Eventos e Formulários com React/fancy-buttons/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+  return (
+    <button>Meu butao</button>
+  );
+}
+}
+
+export default App;
