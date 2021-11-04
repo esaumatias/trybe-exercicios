7841cf536a62c597e53614615e346a74619b778c
Esau Matias <esaufreirematias@gmail.com>
2021-11-04 15:22:05 -0300

    faz exercicios de fixaçao

--- Desenvolvimento Front-end/11 - Componentes com Estado, Eventos e Formulários com React/fancy-buttons/src/App.js
+++ Desenvolvimento Front-end/11 - Componentes com Estado, Eventos e Formulários com React/fancy-buttons/src/App.js
@@ -11,27 +11,37 @@ class App extends React.Component {
     this.evento1 = this.evento1.bind(this);
     this.evento2 = this.evento2.bind(this);
     this.evento3 = this.evento3.bind(this);
+    this.state = {
+      numeroDeClick: 0,
+      numeroDeClick2: 0,
+      numeroDeClick3: 0,
+    }
   }
 
   evento1() {
-    console.log(this)
-    console.log('evento 1');
+    this.setState((anterior, _props) => ({
+      numeroDeClick: anterior.numeroDeClick + 1
+    }))
   }
   
   evento2() {
-    console.log('evento 2');
+    this.setState((anterior, _props) => ({
+      numeroDeClick2: anterior.numeroDeClick2 + 1
+    }))
   }
   
   evento3() {
-    console.log('evento 3');
+    this.setState((anterior, _props) => ({
+      numeroDeClick3: anterior.numeroDeClick3 + 1
+    }))
   }
 
   render() {
   return (
     <div>
-      <button onClick={this.evento1}>butao 1</button>
-      <button onClick={this.evento2}>butao 2</button>
-      <button onClick={this.evento3}>butao 3</button>
+      <button onClick={this.evento1}>{this.state.numeroDeClick}</button>
+      <button onClick={this.evento2}>{this.state.numeroDeClick2}</button>
+      <button onClick={this.evento3}>{this.state.numeroDeClick3}</button>
     </div>
   );
 }
