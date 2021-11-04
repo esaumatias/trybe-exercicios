77c3c728e0d3eb97ff717e6fdd44098a532e74fe
Esau Matias <esaufreirematias@gmail.com>
2021-11-04 14:17:43 -0300

    realiza exercise

--- Desenvolvimento Front-end/11 - Componentes com Estado, Eventos e Formulários com React/fancy-buttons/src/App.js
+++ Desenvolvimento Front-end/11 - Componentes com Estado, Eventos e Formulários com React/fancy-buttons/src/App.js
@@ -2,11 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+
+function evento1() {
+  console.log('evento 1');
+}
+
+function evento2() {
+  console.log('evento 2');
+}
+
+function evento3() {
+  console.log('evento 3');
+}
+
 class App extends React.Component {
 
   render() {
   return (
-    <button>Meu butao</button>
+    <div>
+      <button onClick={evento1}>butao 1</button>
+      <button onClick={evento2}>butao 2</button>
+      <button onClick={evento3}>butao 3</button>
+    </div>
   );
 }
 }
