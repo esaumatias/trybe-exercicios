3eebc11fefa7d31a927f6f9f0401c6771e9c7853
Esau Matias <esaufreirematias@gmail.com>
2021-11-04 14:38:44 -0300

    realiza exercise this

--- Desenvolvimento Front-end/11 - Componentes com Estado, Eventos e Formulários com React/fancy-buttons/src/App.js
+++ Desenvolvimento Front-end/11 - Componentes com Estado, Eventos e Formulários com React/fancy-buttons/src/App.js
@@ -3,26 +3,35 @@ import './App.css';
 import React from 'react';
 
 
-function evento1() {
-  console.log('evento 1');
-}
-
-function evento2() {
-  console.log('evento 2');
-}
-
-function evento3() {
-  console.log('evento 3');
-}
 
 class App extends React.Component {
+
+  constructor() {
+    super()
+    this.evento1 = this.evento1.bind(this);
+    this.evento2 = this.evento2.bind(this);
+    this.evento3 = this.evento3.bind(this);
+  }
+
+  evento1() {
+    console.log(this)
+    console.log('evento 1');
+  }
+  
+  evento2() {
+    console.log('evento 2');
+  }
+  
+  evento3() {
+    console.log('evento 3');
+  }
 
   render() {
   return (
     <div>
-      <button onClick={evento1}>butao 1</button>
-      <button onClick={evento2}>butao 2</button>
-      <button onClick={evento3}>butao 3</button>
+      <button onClick={this.evento1}>butao 1</button>
+      <button onClick={this.evento2}>butao 2</button>
+      <button onClick={this.evento3}>butao 3</button>
     </div>
   );
 }
