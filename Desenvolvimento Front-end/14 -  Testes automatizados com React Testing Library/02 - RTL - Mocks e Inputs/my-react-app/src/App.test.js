a799df4b0d3f7efb2856a84f9344dd0b8ac6daed
Esau Matias <esaufreirematias@gmail.com>
2021-11-26 16:15:10 -0300

    realiza exercicio da aula

--- Desenvolvimento Front-end/14 -  Testes automatizados com React Testing Library/02 - RTL - Mocks e Inputs/my-react-app/src/App.test.js
+++ Desenvolvimento Front-end/14 -  Testes automatizados com React Testing Library/02 - RTL - Mocks e Inputs/my-react-app/src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('verifica se recee piada emostra na tela', async () => {
+  global.fetch = (url) => {
+    return Promise.resolve({
+      json: () => Promise.resolve({
+        id: '7h30Gtr0fxc',
+        joke: 'whinteboards ... are remarkable.',
+        status: 200, 
+      })
+    })
+  }
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const renderJoke = await screen.findByText('whinteboards ... are remarkable.');
+  expect(renderJoke).toBeInTheDocument();
 });
