2e42177e41602327fdeee08362d82555cce216c7
Esau Matias <esaufreirematias@gmail.com>
2022-01-06 15:05:15 -0300

    cria app React e faz configuraçao do redux

---
+++ Desenvolvimento Front-end/15 - Gerenciamento de estado com Redux/04 - Usando o Redux no React - Actions Assíncronas/my-app/src/redux/reducers/reducer.js
@@ -0,0 +1,12 @@
+const INITIAL_VALUE = {
+  store: '',
+}
+
+const reducer = (store = INITIAL_VALUE, action) => {
+  switch (action.type) {
+    default:
+      return store;
+  }
+};
+
+export default reducer;
