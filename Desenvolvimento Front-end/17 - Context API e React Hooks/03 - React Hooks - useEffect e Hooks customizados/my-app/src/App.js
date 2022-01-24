fbdcabc5275b93cec928e21b2f3802261cd07440
Esau Matias <esaufreirematias@gmail.com>
2022-01-24 15:52:21 -0300

    faz exercicios da aula

--- Desenvolvimento Front-end/17 - Context API e React Hooks/03 - React Hooks - useEffect e Hooks customizados/my-app/src/App.js
+++ Desenvolvimento Front-end/17 - Context API e React Hooks/03 - React Hooks - useEffect e Hooks customizados/my-app/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import "./App.css";
 
 function App() {
+  const [pokemons, setPokemons ] = useState([]);
+  const [limit, setLimit ] = useState(10);
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}`;
+
+  useEffect(() => {
+    async function fetchData() {
+      const { results } = await fetch(url).then((response) => response.json());
+      setPokemons(results);
+    }
+    fetchData();
+    return () => {console.log('ola')}
+  }, [limit]);
+
+  const handleMorePokemons = () => {
+    setLimit(limit + 10);
+    console.log(limit);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" onClick={handleMorePokemons}>Buscar mais pokemons</button>
+      {pokemons.map((value, index) => (
+        <div key={index}>
+          <p>{value.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
