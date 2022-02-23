eba601fdc3f70780b9a5ba0d168c79447dd31a42
Esau Matias <esaufreirematias@gmail.com>
2022-02-23 17:41:52 -0300

    adicona exercicios com WHERE

---
+++ Desenvolvimento Back-end/20 - Introdução à SQL/03 - Filtrando dados de forma específica/WHERE COMANDS.sql
@@ -0,0 +1,31 @@
+SELECT * FROM sakila.customer;
+SELECT * FROM sakila.customer
+WHERE active  = FALSE AND store_id = 2 AND first_name <> "KENNETH" ORDER BY first_name;
+
+SELECT * FROM sakila.film;
+SELECT CONCAT(title, ' - ', description, ' - ', release_year, ' - ', replacement_cost ) FROM sakila.film
+WHERE replacement_cost > 18 AND rating <> 'NC-17' ORDER BY replacement_cost DESC LIMIT 100;
+
+SELECT COUNT(active) FROM sakila.customer
+WHERE store_id = 1;
+
+SELECT * FROM sakila.customer
+WHERE active = FALSE AND store_id = 1;
+
+SELECT * FROM sakila.film;
+SELECT * FROM sakila.film
+WHERE rating = 'NC-17' ORDER BY rental_rate LIMIT 50;
+
+SELECT * FROM sakila.film
+WHERE rental_duration BETWEEN 3 and 6;
+
+SELECT * FROM sakila.film
+WHERE rating in('G', 'R');
+
+SELECT * FROM sakila.rental WHERE return_date BETWEEN '2005-05-01 00:00' AND '2005-05-31 23:59:59';
+
+SELECT * FROM sakila.actor WHERE first_name LIKE 'ni%';
+
+
+
+
