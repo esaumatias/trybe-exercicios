e3d7f65dfa7d113335ec127c0392124f84248623
Esau Matias <esaufreirematias@gmail.com>
2022-03-14 18:43:05 -0300

    resolve exercicio 2 de fixaçao do conteudo

--- Desenvolvimento Back-end/Bloco 22 -  Introdução ao desenvolvimento Web com Node.js/02 - Node.js - Fluxo Assíncrono/FluxoAssincrono.js
+++ Desenvolvimento Back-end/Bloco 22 -  Introdução ao desenvolvimento Web com Node.js/02 - Node.js - Fluxo Assíncrono/FluxoAssincrono.js
@@ -1,3 +1,4 @@
+
 function equacao(a, b ,c ) {
   return new Promise((resolve, reject) => {
     if (typeof a !== 'number'|| typeof b !== 'number' || typeof c !== 'number') {
@@ -13,14 +14,30 @@ function equacao(a, b ,c ) {
   });
 }
 
-equacao(1000, 10, 10)
-  .then(resolve => console.log(resolve))
-  .catch(error => console.log(error))
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100 + 1);
+}
 
-equacao(1, 1, 'a')
-  .then(resolve => console.log(resolve))
-  .catch(error => console.log(error))
+async function callDoMath() {
+  const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber);
+try {
+      const result = await equacao(...randomNumbers);
+      console.log(result);
+       } catch (err) {
+         console.error(err);
+       }
+  }
 
-equacao(1, 1, 1)
-  .then(resolve => console.log(resolve))
-  .catch(error => console.log(error))
+  callDoMath()
+
+// equacao(1000, 10, 10)
+//   .then(resolve => console.log(resolve))
+//   .catch(error => console.log(error))
+
+// equacao(1, 1, 'a')
+//   .then(resolve => console.log(resolve))
+//   .catch(error => console.log(error))
+
+// equacao(1, 1, 1)
+//   .then(resolve => console.log(resolve))
+//   .catch(error => console.log(error))
