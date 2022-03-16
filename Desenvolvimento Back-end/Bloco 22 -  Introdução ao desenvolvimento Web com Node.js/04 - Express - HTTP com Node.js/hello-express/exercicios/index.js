6b986998dbd29cd46a11e9276bfa19a0157ee648
Esau Matias <esaufreirematias@gmail.com>
2022-03-16 18:59:49 -0300

    feat: finalisa requisitos 3 e 4 de fixaçao

--- Desenvolvimento Back-end/Bloco 22 -  Introdução ao desenvolvimento Web com Node.js/04 - Express - HTTP com Node.js/hello-express/exercicios/index.js
+++ Desenvolvimento Back-end/Bloco 22 -  Introdução ao desenvolvimento Web com Node.js/04 - Express - HTTP com Node.js/hello-express/exercicios/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
+
+const simpsonsUtils = require('./fs-utils');
 
 const app = express();
+
+const bodyParser = require('body-parser');
+
 
 app.use(bodyParser.json());
 
@@ -15,8 +20,27 @@ app.get('/hello', (req, res) => {
   res.status(200).json({ message: `hello, ${name}!`});
 })
 
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
 
+  if (parseInt(age, 10)> 17) {
+    res.status(200).json({ "message": `Hello, ${name}!` })
+  } else if (parseInt(age, 10) <= 17) {
+    res.status(401).json({ "message": "Unauthorized" })
+  }
+})
 
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+
+  res.status(200).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` })
+})
+
+app.get('/simpsons', rescue(async (req, res) => {
+  const simpsons = await simpsonsUtils.getSimpsons();
+
+  res.status(200).json(simpsons);
+}))
 
 app.use(function (err, req, res, next) {
   res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
