ed91f0ed2d0e08e35e651b6521e5a593fd7cbde2
Esau Matias <esaufreirematias@gmail.com>
2022-03-16 16:02:21 -0300

    feat: implementa exercicos de query string

--- Desenvolvimento Back-end/Bloco 22 -  Introdução ao desenvolvimento Web com Node.js/04 - Express - HTTP com Node.js/hello-express/index.js
+++ Desenvolvimento Back-end/Bloco 22 -  Introdução ao desenvolvimento Web com Node.js/04 - Express - HTTP com Node.js/hello-express/index.js
@@ -21,25 +21,39 @@ const drinks = [
   { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ];
 
-app.get('/recipes/:id', function (req, res) {
-  const { id } = req.params;
-  const recipe = recipes.find((r) => r.id === parseInt(id));
+// app.get('/recipes/:id', function (req, res) {
+//   const { id } = req.params;
+//   const recipe = recipes.find((r) => r.id === parseInt(id));
 
-  if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+//   if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
 
-  res.status(200).json(recipe);
+//   res.status(200).json(recipe);
+// });
+
+// app.get('/drinks/:id', function (req, res) {
+//   const { id } = req.params;
+
+//   const drink = drinks.find((d) => d.id === parseInt(id));
+
+//   if (!drink) return res.status(404).json({ message: 'Drink not found!'});
+
+//   res.status(200).json(drink);
+// })
+
+app.get('/recipes/search', function (req, res) {
+  const { name } = req.query;
+  const filteredRecipes = recipes.filter((r) => r.name.includes(name));
+  res.status(200).json(filteredRecipes);
 });
 
-app.get('/drinks/:id', function (req, res) {
-  const { id } = req.params;
-
-  const drink = drinks.find((d) => d.id === parseInt(id));
-
-  if (!drink) return res.status(404).json({ message: 'Drink not found!'});
-
-  res.status(200).json(drink);
+app.get('/drinks/search', function (req, res) {
+  const { name , minPrice} = req.query;
+  const filterDrinks = drinks.filter((d) => d.name.includes(name) && d.price >= minPrice);
+  res.status(200).json(filterDrinks);
 })
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });
+
+//http://localhost:3001/drinks/search?name=Refrigerante&minPrice=2
