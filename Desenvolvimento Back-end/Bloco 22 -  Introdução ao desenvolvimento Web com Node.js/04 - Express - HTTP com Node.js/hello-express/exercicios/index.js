6aac749e74d8a5383c49ff51e79df7fc1d97f3c8
Esau Matias <esaufreirematias@gmail.com>
2022-03-16 19:03:52 -0300

    feat: finalisa requisitos 5, 6, 7 e 8 de fixaçao

--- Desenvolvimento Back-end/Bloco 22 -  Introdução ao desenvolvimento Web com Node.js/04 - Express - HTTP com Node.js/hello-express/exercicios/index.js
+++ Desenvolvimento Back-end/Bloco 22 -  Introdução ao desenvolvimento Web com Node.js/04 - Express - HTTP com Node.js/hello-express/exercicios/index.js
@@ -42,6 +42,41 @@ app.get('/simpsons', rescue(async (req, res) => {
   res.status(200).json(simpsons);
 }))
 
+app.get(
+  '/simpsons/:id',
+  rescue(async (req, res) => {
+    const simpsons = await simpsonsUtils.getSimpsons();
+
+    const simpson = simpsons.find(({ id }) => id === req.params.id);
+
+    if (!simpson) {
+      return res.status(404).json({ message: 'simpson not found' });
+    }
+
+    return res.status(202).json(simpson);
+  })
+);
+
+app.post(
+  '/simpsons',
+  rescue(async (req, res) => {
+    const { id, name } = req.body;
+
+    const simpsons = await simpsonsUtils.getSimpsons();
+
+    if (simpsons.map(({ id }) => id).includes(id)) {
+      return res.status(409).json({ message: 'id already exists' });
+    }
+
+    simpsons.push({ id, name });
+
+    await simpsonsUtils.setSimpsons(simpsons);
+
+    res.status(204).end();
+  })
+);
+
+
 app.use(function (err, req, res, next) {
   res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
 });
