c41115d77b4d11b03adf5649bfcbb756f54e0bca
Esau Matias <esaufreirematias@gmail.com>
2022-04-06 15:55:48 -0300

    feat: implementa boas práticas

--- Desenvolvimento Back-end/24 - Node.js: ORM e Autenticação/01 - ORM - Interface da aplicação com o banco de dados/app-with-sequelize/models/user.js
+++ Desenvolvimento Back-end/24 - Node.js: ORM e Autenticação/01 - ORM - Interface da aplicação com o banco de dados/app-with-sequelize/models/user.js
@@ -1,11 +1,12 @@
-Copiar
 const User = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
-  fullName: DataTypes.STRING,
-  email: DataTypes.STRING,
-  // aqui inserimos o datatype da coluna criada
-  phone_num: DataTypes.STRING,
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+     // aqui inserimos o datatype da coluna criada
+    phone_num: DataTypes.STRING,
   });
 
   return User;
-}
+};
+
+module.exports = User;
